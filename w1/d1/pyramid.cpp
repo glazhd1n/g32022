7b62ce686c87c7f4fd705369672e5eb33e4ce18a
Sabir <70034235+glazhd1n@users.noreply.github.com>
2022-12-12 16:12:02 +0600

    d1

---
+++ w1/d1/pyramid.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n % 2) {
+        for(int i = 0; i < (n + 1) / 2; i++) {
+            for(int j = 0; j < (n / 2) - i; j++) {
+                cout << ' ';
+            }
+            for(int j = 0; j < 2 * i + 1; j++) {
+                cout << 'X';
+            }
+            for(int j = 0; j < (n / 2) - i; j++) {
+                cout << ' ';
+            }
+            cout << '\n';
+        }
+    } else {
+        // YOUR CODE
+    }
+}
